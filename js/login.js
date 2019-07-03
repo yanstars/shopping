43bbdbf0a6667e32be73f77613ba15defd723097
yanshao <835975379@qq.com>
2019-07-03 20:11:55 +0800

    细节处理

--- js/login.js
+++ js/login.js
@@ -1,21 +1,30 @@
 $(document).ready(function () {
-  $('div[name="same"]').each(function (index, ele) {
 
+
+  //鼠标button内容检测
+  $('button').on('mouseover', function () {
+    setInterval(function () {
+      if ($('input[name=username]').val() != '' && $('input[name=userpsd]').val() != '') {
+        $('button').addClass("ready")
+      } else {
+        $('button').removeClass("ready")
+      }
+    }, 50)
+  })
+//彩蛋随机码
+  var num = Math.floor(Math.random() * 10)
+  // console.log(num)
+
+
+  //漂浮雪花
+  $('div[name="same"]').each(function (index, ele) {
     var x = (Math.random() * 1000)
     var y = (Math.random() * 800)
     var v0x = (Math.random() * 12) - 6
     var v0y = (Math.random() * 8) - 4
     var vx = 0
     var vy = 0
-    $(ele).css({
-      "width": "100px",
-      "height": "100px",
-      "z-index": "1",
-      "position": "absolute",
-      "opacity": "0.7",
-      "background-image": "url('./img/樱花.png')",
-      "background-size": "100% 100%",
-    })
+    $(ele).addClass("snowflake")
     if (index == 0) {
       $(ele).css({
         "width": "82px",
@@ -29,7 +38,7 @@ $(document).ready(function () {
         "animation": " rotatez 15s linear infinite"
       })
 
-    } else if (index == 4) {
+    } else if (index == 3) {
       $(ele).css({
         "width": "128px",
         "height": "128px",
@@ -50,6 +59,15 @@ $(document).ready(function () {
         "animation": " rotatez 5s linear infinite"
       })
     }
+
+
+    if (index == num) {
+      $(ele).on('click', function () {
+        alert("中奖了,喜提彩蛋")
+      })
+    }
+
+
     setInterval(function () {
       vx += v0x
       vy += v0y
@@ -71,5 +89,14 @@ $(document).ready(function () {
         v0y = -v0y
       }
     }, 50)
-  });
+  })
+  //漂浮雪花结束
+
+
+
+
+
+
+
+
 })
