2586b3521a636390caaef66917ab8b5eef91888c
yanshao <835975379@qq.com>
2019-07-03 18:54:34 +0800

    优化动画

--- js/login.js
+++ js/login.js
@@ -7,59 +7,56 @@ $(document).ready(function () {
     var v0y = (Math.random() * 8) - 4
     var vx = 0
     var vy = 0
+    $(ele).css({
+      "width": "100px",
+      "height": "100px",
+      "z-index": "1",
+      "position": "absolute",
+      "opacity": "0.7",
+      "background-image": "url('./img/樱花.png')",
+      "background-size": "100% 100%",
+    })
+    if (index == 0) {
+      $(ele).css({
+        "width": "82px",
+        "height": "64px",
+        "animation": " rotatey 20s linear infinite"
+      })
+    } else if (index == 1) {
+      $(ele).css({
+        "width": "128px",
+        "height": "128px",
+        "animation": " rotatez 15s linear infinite"
+      })
+
+    } else if (index == 4) {
+      $(ele).css({
+        "width": "128px",
+        "height": "128px",
+        "animation": " rotatey 20s linear infinite"
+      })
+
+    } else if (index == 2) {
+      $(ele).css({
+        "width": "175px",
+        "height": "128px",
+        "animation": " rotatez 10s linear infinite",
+      })
+
+    } else {
+      $(ele).css({
+        "width": "64px",
+        "height": "64px",
+        "animation": " rotatez 5s linear infinite"
+      })
+    }
     setInterval(function () {
       vx += v0x
       vy += v0y
       $(ele).css({
         "top": +y + vy + "px",
         "left": +x + vx + "px",
-        "width": "100px",
-        "height": "100px",
-        "z-index": "1",
-        "position": "absolute",
-        "opacity": "0.7",
-        "background-size": "100% 100%",
-        // "box-shadow": "5px 5px  50px 10px sandybrown"
       })
-      if (index == 0) {
-        $(ele).css({
-          "width": "82px",
-          "height": "64px",
-          "background-image": "url('./img/樱花.png')",
-          "animation": " rotatey 20s linear infinite"
-        })
-      } else if (index == 1) {
-        $(ele).css({
-          "width": "128px",
-          "height": "128px",
-          "background-image": "url('./img/樱花.png')",
-          "animation": " rotatez 15s linear infinite"
-        })
-
-      } else if (index == 4) {
-        $(ele).css({
-          "width": "128px",
-          "height": "128px",
-          "background-image": "url('./img/樱花.png')",
-          "animation": " rotatey 20s linear infinite"
-        })
-
-      } else if (index == 2) {
-        $(ele).css({
-          "width": "175px",
-          "height": "128px",
-          "background-image": "url('./img/樱花.png')",
-          "animation": " rotatez 10s linear infinite",
-        })
-
-      } else {
-        $(ele).css({
-          "width": "64px",
-          "height": "64px",
-          "background-image": "url('./img/樱花.png')",
-          "animation": " rotatez 5s linear infinite"
-        })
-      }
 
       if (v0x < 0 && $(ele).offset().left <= 0) {
         v0x = -v0x
--- js/resgin.js
+++ js/resgin.js
@@ -1,66 +1,63 @@
 $(document).ready(function () {
   $('div[name="same"]').each(function (index, ele) {
+
     var x = (Math.random() * 1000)
     var y = (Math.random() * 800)
     var v0x = (Math.random() * 12) - 6
     var v0y = (Math.random() * 8) - 4
     var vx = 0
     var vy = 0
+    $(ele).css({
+      "width": "100px",
+      "height": "100px",
+      "z-index": "1",
+      "position": "absolute",
+      "opacity": "0.7",
+      "background-image": "url('./img/樱花.png')",
+      "background-size": "100% 100%",
+    })
+    if (index == 0) {
+      $(ele).css({
+        "width": "82px",
+        "height": "64px",
+        "animation": " rotatey 20s linear infinite"
+      })
+    } else if (index == 1) {
+      $(ele).css({
+        "width": "128px",
+        "height": "128px",
+        "animation": " rotatez 15s linear infinite"
+      })
 
+    } else if (index == 4) {
+      $(ele).css({
+        "width": "128px",
+        "height": "128px",
+        "animation": " rotatey 20s linear infinite"
+      })
+
+    } else if (index == 2) {
+      $(ele).css({
+        "width": "175px",
+        "height": "128px",
+        "animation": " rotatez 10s linear infinite",
+      })
+
+    } else {
+      $(ele).css({
+        "width": "64px",
+        "height": "64px",
+        "animation": " rotatez 5s linear infinite"
+      })
+    }
     setInterval(function () {
       vx += v0x
       vy += v0y
       $(ele).css({
         "top": +y + vy + "px",
         "left": +x + vx + "px",
-        "width": "100px",
-        "height": "100px",
-        "z-index": "1",
-        "background-image": "url('./img/樱花.png')",
-        "position": "absolute",
-        "opacity": "0.7",
-        "background-size": "100% 100%",
-
       })
-      if (index == 0) {
-        $(ele).css({
-          "width": "82px",
-          "height": "64px",
-          "background-image": "url('./img/樱花.png')",
-          "animation": " rotateZ 2s linear infinite"
-        })
-      } else if (index == 1) {
-        $(ele).css({
-          "width": "128px",
-          "height": "128px",
-          "background-image": "url('./img/樱花.png')",
-          "animation": " rotatez 10s linear infinite"
-        })
 
-      } else if (index == 4) {
-        $(ele).css({
-          "width": "128px",
-          "height": "128px",
-          "background-image": "url('./img/樱花.png')",
-          "animation": " rotatez 8s linear infinite"
-        })
-
-      } else if (index == 2) {
-        $(ele).css({
-          "width": "175px",
-          "height": "128px",
-          "background-image": "url('./img/樱花.png')",
-          "animation": " rotatez 10s linear infinite"
-        })
-
-      } else {
-        $(ele).css({
-          "width": "64px",
-          "height": "64px",
-          "background-image": "url('./img/樱花.png')",
-          "animation": " rotatez 30s linear infinite"
-        })
-      }
       if (v0x < 0 && $(ele).offset().left <= 0) {
         v0x = -v0x
       }
@@ -74,7 +71,5 @@ $(document).ready(function () {
         v0y = -v0y
       }
     }, 50)
-
   });
-
 })
